1492e3a6f17049e410f9eb33a0dfecd53a8ce488
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-09 18:25:49 +0530

    feat [auth]: Add AppDimens to handling dimensions

---
+++ lib/constants/app_dimensions.dart
@@ -0,0 +1,55 @@
+class AppDimens {
+  // Padding
+  static const double paddingXSmall = 2.0;
+  static const double paddingSmall = 4.0;
+  static const double paddingRegular = 8.0;
+  static const double paddingMedium = 16.0;
+  static const double paddingLarge = 24.0;
+  static const double paddingXLarge = 32.0;
+
+  // Spacing
+  static const double spaceXSmall = 2.0;
+  static const double spaceSmall = 4.0;
+  static const double spaceRegular = 8.0;
+  static const double spaceMedium = 16.0;
+  static const double spaceLarge = 24.0;
+  static const double spaceXLarge = 32.0;
+
+  // Text Sizes
+  static const double textSizeXSmall = 10.0;
+  static const double textSizeSmall = 12.0;
+  static const double textSizeMedium = 16.0;
+  static const double textSizeLarge = 18.0;
+  static const double textSizeXLarge = 24.0;
+  static const double textSizeTitle = 32.0;
+
+  // Elevations
+  static const double elevationSmall = 2.0;
+  static const double elevationMedium = 4.0;
+  static const double elevationLarge = 8.0;
+  static const double elevationXLarge = 16.0;
+
+  // Corner Radius
+  static const double radiusXSmall = 2.0;
+  static const double radiusSmall = 4.0;
+  static const double radiusMedium = 8.0;
+  static const double radiusLarge = 16.0;
+
+  // Icon Sizes
+  static const double iconSizeXSmall = 16.0;
+  static const double iconSizeSmall = 24.0;
+  static const double iconSizeMedium = 32.0;
+  static const double iconSizeLarge = 48.0;
+  static const double iconSizeXLarge = 64.0;
+
+  // General Sizes for custom components
+  static const double sizeXSmall = 8.0;
+  static const double sizeSmall = 24.0;
+  static const double sizeMedium = 32.0;
+  static const double sizeLarge = 48.0;
+  static const double sizeXLarge = 64.0;
+  static const double size2XLarge = 96.0;
+  static const double size3XLarge = 128.0;
+
+
+}
