4d1622335992864e58e4044bb454ebfd3b895141
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-09 17:26:52 +0530

    feat [auth]: Add custom widget > SpacerUnit

---
+++ lib/features/auth/presentation/components/spacer_unit.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// A widget that adds space with customizable [height] and [width].
+class SpacerUnit extends StatelessWidget {
+  final double height;
+  final double width;
+
+  const SpacerUnit({this.height = 0.0, this.width = 0.0, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: height, width: width);
+  }
+}
