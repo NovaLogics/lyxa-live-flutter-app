302f1e0f070925efc0902adfd9769ba43bb7102c
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-09 18:26:07 +0530

    feat [auth]: Add AppDimens > Custom Sizes

--- lib/constants/app_dimensions.dart
+++ lib/constants/app_dimensions.dart
@@ -52,4 +52,30 @@ class AppDimens {
   static const double size3XLarge = 128.0;
 
 
+ // Custom Sizes
+  static const double size4 = 4.0;
+  static const double size8 = 8.0;
+  static const double size12 = 12.0;
+  static const double size16 = 16.0;
+  static const double size20 = 20.0;
+  static const double size24 = 24.0;
+  static const double size28 = 28.0;
+  static const double size32 = 32.0;
+  static const double size36 = 36.0;
+  static const double size40 = 40.0;
+  static const double size44 = 44.0;
+  static const double size48 = 48.0;
+  static const double size52 = 52.0;
+  static const double size56 = 56.0;
+  static const double size60 = 60.0;
+  static const double size64 = 64.0;
+  static const double size68 = 68.0;
+  static const double size72 = 72.0;
+  static const double size76 = 76.0;
+  static const double size80 = 80.0;
+  static const double size84 = 84.0;
+  static const double size88 = 88.0;
+  static const double size92 = 92.0;
+  static const double size96 = 96.0;
+  static const double size100 = 100.0;
 }
