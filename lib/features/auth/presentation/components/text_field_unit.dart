5db193a71232354bab3bda81ed3f9490429df861
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-09 17:21:45 +0530

    feat [auth]: Add custom widget > TextFieldUnit

---
+++ lib/features/auth/presentation/components/text_field_unit.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class TextFieldUnit extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+
+  const TextFieldUnit({
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
+  }
+}
