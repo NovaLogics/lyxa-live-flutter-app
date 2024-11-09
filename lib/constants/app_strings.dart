23c00eb5b7b16a28dd8f9bb6acb4175e1e2eafe0
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-09 18:07:16 +0530

    feat [auth]: Add AppStrings to handling strings

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,10 @@
+class AppStrings {
+  static const String appName = 'LYXA Live';
+  static const String welcomeMessage = 'Welcome to LYXA Live';
+  static const String errorMessage = 'Something went wrong';
+
+  // Login Section
+  static const String welcomeBackMessage = 'Welcome Back, You\'ve been missed!';
+  static const String email = 'Email';
+  static const String password = 'Password';
+}
