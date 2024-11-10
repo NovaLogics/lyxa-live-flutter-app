ad3b566e19da4e9846bb07b859b3fa73880b547c
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-10 15:29:10 +0530

    feat [profile]: Add fetch User Profile funtion in FirebaseProfileRepository

---
+++ lib/features/profile/data/firebase_profile_repository.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lyxa_live/constants/constants.dart';
+import 'package:lyxa_live/features/profile/domain/entities/profile_user.dart';
+import 'package:lyxa_live/features/profile/domain/repositories/profile_repository.dart';
+
+class FirebaseProfileRepository implements ProfileRepository {
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
+  @override
+  Future<ProfileUser?> fetchUserProfile(String uid) async {
+    try {
+      // Get user document from Firestore
+      final userDoc =
+          await firebaseFirestore.collection(dbPathUsers).doc(uid).get();
+
+      if (userDoc.exists) {
+        final userData = userDoc.data();
+
+        if (userData != null) {
+          return ProfileUser(
+            uid: uid,
+            email: userData['email'],
+            name: userData['name'],
+            bio: userData['bio'],
+            profileImageUrl: userData['profileImageUrl'].toString(),
+          );
+        }
+      }
+      return null;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  @override
+  Future<void> updateProfile(ProfileUser updatedProfile) {
+    // TODO: implement updateProfile
+    throw UnimplementedError();
+  }
+}
