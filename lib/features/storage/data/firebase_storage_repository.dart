be8d9dcf1f9c7797cc5f7fe7d94b26e544500a38
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-10 19:02:46 +0530

    feat [storage]: Add Mobile platforms file upload helper function> FirebaseStorageRepository

--- lib/features/storage/data/firebase_storage_repository.dart
+++ lib/features/storage/data/firebase_storage_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:lyxa_live/features/storage/domain/storage_repository.dart';
 
@@ -14,4 +16,30 @@ class FirebaseStorageRepository implements StorageRepository {
     // TODO: implement uploadProfileImageWeb
     throw UnimplementedError();
   }
+
+  // Helper methods : to upload files to storage
+
+  // Mobile platforms (file)
+  Future<String?> _uploadFile(
+      String path, String fileName, String folder) async {
+    try {
+      // Get file
+      final file = File(path);
+
+      // Find place to store
+      final storageRef = storage.ref().child('$folder/$fileName');
+
+      // Upload
+      final uploadTask = await storageRef.putFile(file);
+
+      // Get image download url
+      final downloadUrl = await uploadTask.ref.getDownloadURL();
+
+      return downloadUrl;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  // Web platforms (file)
 }
