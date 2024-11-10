d92c543c195b8308a4b2ce6c07c760935d847d49
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-10 16:37:50 +0530

    feat [profile]: Add Profile screen with BlocBuilder, cubit

--- lib/features/profile/presentation/screens/profile_screen.dart
+++ lib/features/profile/presentation/screens/profile_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lyxa_live/features/auth/domain/entities/app_user.dart';
 import 'package:lyxa_live/features/auth/presentation/cubits/auth_cubit.dart';
+import 'package:lyxa_live/features/profile/presentation/cubits/profile_cubit.dart';
+import 'package:lyxa_live/features/profile/presentation/cubits/profile_state.dart';
 
 class ProfileScreen extends StatefulWidget {
   final String uid;
@@ -15,17 +17,50 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   // Cubits
   late final authCubit = context.read<AuthCubit>();
+  late final profileCubit = context.read<ProfileCubit>();
 
   // Current user
   late AppUser? currentUser = authCubit.currentUser;
 
+  // On Startup
+  @override
+  void initState() {
+    super.initState();
+
+    // Load user profile data
+    profileCubit.fetchUserProfile(widget.uid);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(currentUser!.email),
-        foregroundColor: Theme.of(context).colorScheme.primary,
-      ),
+    return BlocBuilder<ProfileCubit, ProfileState>(
+      builder: (context, state) {
+        // Loaded
+        if (state is ProfileLoaded) {
+          final user = state.profileUser;
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(user.name),
+              foregroundColor: Theme.of(context).colorScheme.primary,
+            ),
+          );
+        }
+        // Loading
+        else if (state is ProfileLoading) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        // Other conditions
+        else {
+          return const Center(
+            child: Text("No profile found..."),
+          );
+        }
+      },
     );
   }
 }
