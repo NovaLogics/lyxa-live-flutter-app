0c4a34d845fe1f81daf880220e6ad71af8ba0f0c
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-10 18:50:14 +0530

    feat [storage]: Add Repo > FirebaseStorageRepository

---
+++ lib/features/storage/data/firebase_storage_repository.dart
@@ -0,0 +1,16 @@
+import 'package:lyxa_live/features/storage/domain/storage_repository.dart';
+
+class FirebaseStorageRepository implements StorageRepository{
+  @override
+  Future<String?> uploadProfileImageMobile(String path, String fileName) {
+    // TODO: implement uploadProfileImageMobile
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String?> uploadProfileImageWeb(String fileBytes, String fileName) {
+    // TODO: implement uploadProfileImageWeb
+    throw UnimplementedError();
+  }
+
+}
