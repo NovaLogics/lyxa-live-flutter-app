5c697e7ae130324523ccb0d5bd49ce22fa918c7e
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-11 15:50:30 +0530

    feat [post]: Implement Repo.Method > FirebasePostRepository > createPost

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: constant_identifier_names
 
 const String FIRESTORE_COLLECTION_USERS = 'users';
+const String FIRESTORE_COLLECTION_POSTS = 'posts';
 const String STORAGE_PATH_PROFILE_IMAGES = 'profile_images';
 
--- lib/features/post/data/firebase_post_repository.dart
+++ lib/features/post/data/firebase_post_repository.dart
@@ -1,11 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lyxa_live/constants/constants.dart';
 import 'package:lyxa_live/features/post/domain/entities/post.dart';
 import 'package:lyxa_live/features/post/domain/repositories/post_repository.dart';
 
-class FirebasePostRepository implements PostRepository{
+class FirebasePostRepository implements PostRepository {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  // Path Firebase > posts
+  final CollectionReference postCollection =
+      FirebaseFirestore.instance.collection(FIRESTORE_COLLECTION_POSTS);
+
   @override
-  Future<void> createPost(Post post) {
-    // TODO: implement createPost
-    throw UnimplementedError();
+  Future<void> createPost(Post post) async {
+    try {
+      await postCollection.doc(post.id).set(post.toJson());
+    } catch (error) {
+      throw Exception('Error creating post ${error.toString()}');
+    }
   }
 
   @override
