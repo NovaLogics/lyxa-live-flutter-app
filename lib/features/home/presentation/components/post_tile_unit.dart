8af4a052977d10654d15d655cf237ca5a29749c4
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-11 20:50:17 +0530

    feat [home]: Add Custom Widget > PostTileUnit

---
+++ lib/features/home/presentation/components/post_tile_unit.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PostTileUnit extends StatelessWidget {
+  const PostTileUnit({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
