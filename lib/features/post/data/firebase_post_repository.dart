d63d22b9c019e39ec81a5db7687cecf7ace6a287
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-11 15:58:31 +0530

    feat [post]: Implement Repo.Method > FirebasePostRepository > fetchAllPosts

--- lib/features/post/data/firebase_post_repository.dart
+++ lib/features/post/data/firebase_post_repository.dart
@@ -15,23 +15,35 @@ class FirebasePostRepository implements PostRepository {
     try {
       await postCollection.doc(post.id).set(post.toJson());
     } catch (error) {
-      throw Exception('Error creating post ${error.toString()}');
+      throw Exception('Error creating post : ${error.toString()}');
     }
   }
 
   @override
-  Future<void> deletePost(String postId) async{
+  Future<void> deletePost(String postId) async {
     try {
       await postCollection.doc(postId).delete();
     } catch (error) {
-      throw Exception('Error deleting post ${error.toString()}');
+      throw Exception('Error deleting post : ${error.toString()}');
     }
   }
 
   @override
-  Future<List<Post>> fetchAllPosts() {
-    // TODO: implement fetchAllPosts
-    throw UnimplementedError();
+  Future<List<Post>> fetchAllPosts() async {
+    try {
+      // Get all posts with most recent posts at the top
+      final postSnapshot =
+          await postCollection.orderBy('timestamp', descending: true).get();
+
+      // Convert each firestore document from json -> list of post
+      final List<Post> allPosts = postSnapshot.docs
+          .map((doc) => Post.fromJson(doc.data() as Map<String, dynamic>))
+          .toList();
+
+      return allPosts;
+    } catch (error) {
+      throw Exception('Error fetching post : ${error.toString()}');
+    }
   }
 
   @override
