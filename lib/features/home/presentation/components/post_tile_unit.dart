923d5dac1b73b3a658c373d0e3702c36758711d4
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-11 22:08:38 +0530

    feat [home]: Impl PostTileUnit { FUN } | base Improve UI designs, display profile pic, name , dlete button, image

--- lib/features/home/presentation/components/post_tile_unit.dart
+++ lib/features/home/presentation/components/post_tile_unit.dart
@@ -1,6 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lyxa_live/features/auth/domain/entities/app_user.dart';
+import 'package:lyxa_live/features/auth/presentation/cubits/auth_cubit.dart';
 import 'package:lyxa_live/features/post/domain/entities/post.dart';
+import 'package:lyxa_live/features/post/presentation/cubits/post_cubit.dart';
+import 'package:lyxa_live/features/profile/domain/entities/profile_user.dart';
+import 'package:lyxa_live/features/profile/presentation/cubits/profile_cubit.dart';
 
 class PostTileUnit extends StatefulWidget {
   final Post post;
@@ -17,6 +23,42 @@ class PostTileUnit extends StatefulWidget {
 }
 
 class _PostTileUnitState extends State<PostTileUnit> {
+  // Cubits
+  late final postCubit = context.read<PostCubit>();
+  late final profileCubit = context.read<ProfileCubit>();
+
+  bool isOwnPost = false;
+
+  // Current user
+  AppUser? currentUser;
+
+  // Post user
+  ProfileUser? postUser;
+
+  // On startup
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentUser();
+    fetchPostUser();
+  }
+
+  void getCurrentUser() {
+    final authCubit = context.read<AuthCubit>();
+    currentUser = authCubit.currentUser;
+    isOwnPost = (widget.post.userId == currentUser!.uid);
+  }
+
+  void fetchPostUser() async {
+    final fetchedUser = await profileCubit.getUserProfile(widget.post.userId);
+    if (fetchedUser != null) {
+      setState(() {
+        postUser = fetchedUser;
+      });
+    }
+  }
+
   // Show options for deletion
   void showOptions() {
     showDialog(
@@ -47,39 +89,81 @@ class _PostTileUnitState extends State<PostTileUnit> {
   // Build UI
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            //Name
-            Text(widget.post.userName),
-            // Delete button
-            IconButton(
-              onPressed: showOptions,
-              icon: const Icon(Icons.delete),
+    return Container(
+      color: Theme.of(context).colorScheme.secondary,
+      child: Column(
+        children: [
+          // Top section
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Profile Pic
+                postUser?.profileImageUrl != null
+                    ? CachedNetworkImage(
+                        imageUrl: postUser!.profileImageUrl,
+                        errorWidget: (context, url, error) => Icon(
+                              Icons.person,
+                              color:
+                                  Theme.of(context).colorScheme.inversePrimary,
+                            ),
+                        imageBuilder: (context, imageProvider) => Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: imageProvider,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ))
+                    : const Icon(Icons.person),
+                const SizedBox(width: 12),
+                //Name
+                Text(
+                  widget.post.userName,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Spacer(),
+                // Delete button
+                if (isOwnPost)
+                  GestureDetector(
+                    onDoubleTap: showOptions,
+                    child: Icon(
+                      Icons.delete,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+              ],
             ),
-          ],
-        ),
-        CachedNetworkImage(
-          imageUrl: widget.post.imageUrl,
-          height: 430,
-          width: double.infinity,
-          fit: BoxFit.cover,
-          // Loading
-          placeholder: (context, url) => const SizedBox(
-            height: 430,
-          ),
-          // Error -> Failed to load
-          errorWidget: (context, url, error) => Icon(
-            Icons.error,
-            color: Theme.of(context).colorScheme.primary,
           ),
 
-          errorListener: (value) => ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text(value.toString()))),
-        ),
-      ],
+          // Image
+          CachedNetworkImage(
+            imageUrl: widget.post.imageUrl,
+            height: 430,
+            width: double.infinity,
+            fit: BoxFit.cover,
+            // Loading
+            placeholder: (context, url) => const SizedBox(
+              height: 430,
+            ),
+            // Error -> Failed to load
+            errorWidget: (context, url, error) => Icon(
+              Icons.error,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+
+            errorListener: (value) => ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text(value.toString()))),
+          ),
+        ],
+      ),
     );
   }
 }
