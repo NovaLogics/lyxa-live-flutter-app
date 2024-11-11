60c38b8e1df45f0da032bb37796d7ef5c86758da
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-11 20:17:46 +0530

    feat [home]: Add UploadPost button and attach UploadPostScreen

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lyxa_live/constants/app_strings.dart';
 import 'package:lyxa_live/features/home/presentation/components/drawer_unit.dart';
+import 'package:lyxa_live/features/post/presentation/screens/upload_post_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,9 +15,21 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        foregroundColor: Theme.of(context).colorScheme.primary,
         title: const Text(
           AppStrings.appName,
         ),
+        actions: [
+          // Upload new post button
+          IconButton(
+            onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const UploadPostScreen(),
+                )),
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       drawer: const DrawerUnit(),
     );
