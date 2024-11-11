de3c5874dbc18851cad90749c0e1c1792accd24c
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-11 17:12:22 +0530

    feat [post]: Add Screen > UploadPostScreen

---
+++ lib/features/post/presentation/screens/upload_post_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class UploadPostScreen extends StatefulWidget {
+  const UploadPostScreen({super.key});
+
+  @override
+  State<UploadPostScreen> createState() => _UploadPostScreenState();
+}
+
+class _UploadPostScreenState extends State<UploadPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
