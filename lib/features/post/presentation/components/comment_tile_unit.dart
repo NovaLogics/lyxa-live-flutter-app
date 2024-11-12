eea84e73ca15eaa9a926ed4187f4a4c88d990e21
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-12 15:44:00 +0530

    feat [post]: Impl UI -> CommentTileUnit -> Base UI designs

--- lib/features/post/presentation/components/comment_tile_unit.dart
+++ lib/features/post/presentation/components/comment_tile_unit.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:lyxa_live/features/post/domain/entities/comment.dart';
 
-class CommentTileUnit extends StatelessWidget {
-  const CommentTileUnit({super.key});
+class CommentTileUnit extends StatefulWidget {
+  final Comment comment;
+
+  const CommentTileUnit({
+    super.key,
+    required this.comment,
+  });
 
   @override
+  State<CommentTileUnit> createState() => _CommentTileUnitState();
+}
+
+class _CommentTileUnitState extends State<CommentTileUnit> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0),
+      child: Row(
+        children: [
+          // Username
+          Text(
+            widget.comment.userName,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(width: 10),
+
+          // Comment Text
+          Text(
+            widget.comment.text,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 12,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+
+          // Delete button
+        ],
+      ),
+    );
   }
 }
