9191f9d707481919882f671455da753e7f097044
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-12 16:03:00 +0530

    feat [post]: Impl UI -> CommentTileUnit -> Initial design and delete button

--- lib/features/post/presentation/components/comment_tile_unit.dart
+++ lib/features/post/presentation/components/comment_tile_unit.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lyxa_live/features/auth/domain/entities/app_user.dart';
+import 'package:lyxa_live/features/auth/presentation/cubits/auth_cubit.dart';
 import 'package:lyxa_live/features/post/domain/entities/comment.dart';
+import 'package:lyxa_live/features/post/presentation/cubits/post_cubit.dart';
 
 class CommentTileUnit extends StatefulWidget {
   final Comment comment;
@@ -14,10 +18,58 @@ class CommentTileUnit extends StatefulWidget {
 }
 
 class _CommentTileUnitState extends State<CommentTileUnit> {
+  // Current user
+  AppUser? currentUser;
+  bool isOwnPost = false;
+
+  // On startup
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    final authCubit = context.read<AuthCubit>();
+    currentUser = authCubit.currentUser;
+    isOwnPost = (widget.comment.userId == currentUser!.uid);
+  }
+
+  // Show options for deletion
+  void showOptions() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Delete Comment"),
+        actions: [
+          // Cancel button
+          TextButton(
+            onPressed: () {
+              Navigator.of(context, rootNavigator: true).pop('dialog');
+            },
+            child: const Text("Cancel"),
+          ),
+          // Delete button
+          TextButton(
+            onPressed: () {
+              context.read<PostCubit>().deleteComment(
+                    widget.comment.postId,
+                    widget.comment.id,
+                  );
+              Navigator.of(context).pop();
+            },
+            child: const Text("Delete"),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 20.0),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
       child: Row(
         children: [
           // Username
@@ -41,7 +93,19 @@ class _CommentTileUnitState extends State<CommentTileUnit> {
             ),
           ),
 
+          const Spacer(),
+
           // Delete button
+          if (isOwnPost)
+            GestureDetector(
+              onTap: showOptions,
+              child: AbsorbPointer(
+                child: Icon(
+                  Icons.more_horiz,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ),
         ],
       ),
     );
