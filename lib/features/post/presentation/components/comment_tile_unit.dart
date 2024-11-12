f9e18b3bd73a5199813998bdb9babe1c9c384eb7
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-12 15:37:33 +0530

    feat [post]: Add Widget > CommentTileUnit

---
+++ lib/features/post/presentation/components/comment_tile_unit.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommentTileUnit extends StatelessWidget {
+  const CommentTileUnit({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
