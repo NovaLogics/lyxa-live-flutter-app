1bb1dc1160cb03207f11a11091b5135524a5c60e
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-12 18:39:21 +0530

    feat [profile]: Add Widget > ProfileStatsUnit

---
+++ lib/features/profile/presentation/components/profile_stats_unit.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ProfileStatsUnit extends StatelessWidget {
+  final int postCount;
+  final int followerCount;
+  final int followingCount;
+
+  const ProfileStatsUnit({
+    super.key,
+    required this.postCount,
+    required this.followerCount,
+    required this.followingCount,
+  });
+
+// Build UI
+  @override
+  Widget build(BuildContext context) {
+    var textStyleForCount = TextStyle(
+      color: Theme.of(context).colorScheme.inversePrimary,
+      fontWeight: FontWeight.normal,
+      fontSize: 20,
+    );
+
+    var textStyleForText = TextStyle(
+      color: Theme.of(context).colorScheme.primary,
+      fontWeight: FontWeight.normal,
+      fontSize: 16,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        // Post
+        SizedBox(
+          width: 100,
+          child: Column(
+            children: [
+              Text(
+                postCount.toString(),
+                style: textStyleForCount,
+              ),
+              Text(
+                "Posts",
+                style: textStyleForText,
+              ),
+            ],
+          ),
+        ),
+
+        // Followers
+        SizedBox(
+          width: 100,
+          child: Column(
+            children: [
+              Text(
+                followerCount.toString(),
+                style: textStyleForCount,
+              ),
+              Text(
+                "Followers",
+                style: textStyleForText,
+              ),
+            ],
+          ),
+        ),
+
+        // Following
+        SizedBox(
+          width: 100,
+          child: Column(
+            children: [
+              Text(
+                followingCount.toString(),
+                style: textStyleForCount,
+              ),
+              Text(
+                "Following",
+                style: textStyleForText,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
