9aa9dcbc5240ba2a7d13fd2667689c13d2ed432c
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-12 18:05:48 +0530

    feat [profile]: Impl UI -> FollowButtonUnit -> Base UI designs

--- lib/features/profile/presentation/components/follow_button_unit.dart
+++ lib/features/profile/presentation/components/follow_button_unit.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
 class FollowButtonUnit extends StatelessWidget {
-  const FollowButtonUnit({super.key});
+  final void Function()? onPressed;
+  final bool isFollowing;
 
+  const FollowButtonUnit({
+    super.key,
+    required this.onPressed,
+    required this.isFollowing,
+  });
+
+  // Build UI
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      // Padding on outside
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: MaterialButton(
+          padding: const EdgeInsets.all(12),
+          onPressed: onPressed,
+          color: isFollowing
+              ? Theme.of(context).colorScheme.inversePrimary
+              : Colors.blueAccent,
+          child: Text(
+            isFollowing ? "Unfollow" : "Follow",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.secondary,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
