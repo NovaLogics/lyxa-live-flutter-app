8dc556bdec60f558f44458c353332177358dcbd0
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-14 02:33:04 +0530

    feat [ theme ]: Update dark mode colours

--- lib/src/core/themes/dark_mode.dart
+++ lib/src/core/themes/dark_mode.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData darkMode = ThemeData(
   colorScheme: const ColorScheme.dark(
     surface: Color.fromARGB(255, 9, 9, 9),
-    primary: Colors.white70,
+    primary: Color.fromARGB(218, 255, 255, 255),
     secondary: Color.fromARGB(255, 20, 20, 20),
     tertiary: Color.fromARGB(255, 29, 29, 29),
     inversePrimary: Colors.white,
