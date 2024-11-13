40e93cf66b19738ea4ac610f292f85b05171e78d
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-13 14:19:14 +0530

    refactor [ main ]: Update main file code structure

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,25 @@ import 'package:lyxa_live/src/app.dart';
 import 'package:lyxa_live/src/config/firebase_options.dart';
 
 void main() async {
+  // Ensures all Flutter bindings are initialized
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Run the setup process
+  await setupFirebase();
+
+  // Launch the app
+  startApp();
+}
+
+Future<void> setupFirebase() async {
+  // Initializes Firebase with platform-specific options
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+}
 
+void startApp() {
+  // Starts the app by running the root widget
   runApp(LyxaApp());
 }
+
