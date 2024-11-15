292f69e695e5387c62a139fee1f04e6f353dd937
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-15 18:01:55 +0530

    feat [ core-values ]: Update App color values

--- lib/src/core/values/app_colors.dart
+++ lib/src/core/values/app_colors.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const Color blueGreyShade900 = Color(0xFF263238);
+  static const Color deepPurpleShade700 = Color(0xFF512DA8);
+}
+
+class AppThemeColors {
   static const Color primary = Color(0xFF6200EA);
   static const Color accent = Color(0xFF03DAC5);
   static const Color background = Color(0xFFF6F6F6);
