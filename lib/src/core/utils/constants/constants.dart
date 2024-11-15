c3e2036a293d8316291ddcd9d2968febd110b9e9
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-16 02:01:57 +0530

    feat [ core-constants ]: Add constants > Max length values

--- lib/src/core/utils/constants/constants.dart
+++ lib/src/core/utils/constants/constants.dart
@@ -9,3 +9,12 @@ const String IMAGE_PATH_LYXA_BANNER = 'assets/images/lyxa_banner.png';
 
 const String FONT_RALEWAY = 'Raleway';
 
+const int MAX_LENGTH_EMAIL_FIELD = 128;
+const int MAX_LENGTH_USERNAME_FIELD = 30;
+const int MAX_LENGTH_PASSWORD_FIELD = 15;
+const int MAX_LENGTH_FULL_NAME_FIELD = 100;
+const int MAX_LENGTH_BIO_DESCRIPTION_FIELD = 300;
+const int MAX_LENGTH_COMMENTS_FIELD = 500;
+const int MAX_LENGTH_POST_FIELD = 500;
+
+
