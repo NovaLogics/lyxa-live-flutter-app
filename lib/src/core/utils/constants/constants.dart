b86719342ecffa86eae47dfd984031ae4ace314d
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-15 18:22:35 +0530

    feat [ core-constant ]: Update App constant file

--- lib/src/core/utils/constants/constants.dart
+++ lib/src/core/utils/constants/constants.dart
@@ -5,3 +5,7 @@ const String FIRESTORE_COLLECTION_POSTS = 'posts';
 const String STORAGE_PATH_PROFILE_IMAGES = 'profile_images';
 const String STORAGE_PATH_POST_IMAGES = 'post_images';
 
+const String IMAGE_PATH_LYXA_BANNER = 'assets/images/lyxa_banner.png';
+
+const String FONT_RALEWAY = 'Raleway';
+
