14a33d3a207f49ea3f7d9fde874e285726404a44
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-16 01:13:24 +0530

    feat [ auth ]: Impl Screen [ AuthScreen ] > Add loading screen

--- lib/src/features/auth/ui/screens/auth_screen.dart
+++ lib/src/features/auth/ui/screens/auth_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lyxa_live/src/core/values/app_dimensions.dart';
+import 'package:lyxa_live/src/features/auth/domain/entities/app_user.dart';
 import 'package:lyxa_live/src/features/auth/ui/components/scrollable_scaffold.dart';
 import 'package:lyxa_live/src/features/auth/ui/screens/login_screen.dart';
 import 'package:lyxa_live/src/features/auth/ui/screens/register_screen.dart';
+import 'package:lyxa_live/src/shared/widgets/center_loading_unit.dart';
 import 'package:lyxa_live/src/shared/widgets/gradient_background_unit.dart';
 
-/// AuthScreen: 
+/// AuthScreen:
 /// -> Displays either the Login or Register page with the ability to toggle between them
 class AuthScreen extends StatefulWidget {
-  const AuthScreen({super.key});
+  final AppUser? authUser;
+  final bool isLoading;
+
+  const AuthScreen({
+    super.key,
+    this.authUser,
+    this.isLoading = false,
+  });
 
   @override
   State<AuthScreen> createState() => _AuthScreenState();
@@ -25,7 +34,7 @@ class _AuthScreenState extends State<AuthScreen> {
     });
   }
 
-    /// Set status bar color styles
+  /// Set status bar color styles
   void _setStatusBarStyle() {
     SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(
@@ -42,12 +51,19 @@ class _AuthScreenState extends State<AuthScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          const GradientBackgroundUnit(width: AppDimens.containerSize400), 
+          const GradientBackgroundUnit(width: AppDimens.containerSize400),
           ScrollableScaffold(
             body: _isLoginPage
-                ? LoginScreen(onToggle: _toggleAuthenticationPage)
-                : RegisterScreen(onToggle: _toggleAuthenticationPage),
+                ? LoginScreen(
+                    onToggle: _toggleAuthenticationPage,
+                    authUser: widget.authUser,
+                  )
+                : RegisterScreen(
+                    onToggle: _toggleAuthenticationPage,
+                    authUser: widget.authUser,
+                  ),
           ),
+          if (widget.isLoading) const CenterLoadingUnit(),
         ],
       ),
     );
