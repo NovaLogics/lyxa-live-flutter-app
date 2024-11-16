ef570be4e2dc2f839a2819dde2909428c4843e4f
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-16 16:30:53 +0530

    feat [ res-theme ]: Update dark mode colours

--- lib/src/core/themes/dark_mode.dart
+++ lib/src/core/themes/dark_mode.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:lyxa_live/src/core/values/app_colors.dart';
 
 ThemeData darkMode = ThemeData(
-  colorScheme: const ColorScheme.dark(
-    surface: Color.fromARGB(255, 9, 9, 9),
-    primary: Color.fromARGB(194, 255, 255, 255),
-    secondary: Color.fromARGB(255, 20, 20, 20),
-    tertiary: Color.fromARGB(255, 29, 29, 29),
-    inversePrimary: Colors.white,
-    surfaceContainerLow : Color.fromARGB(175, 20, 20, 20),
-  ),
-  scaffoldBackgroundColor: const Color.fromARGB(255, 9, 9, 9),
+  colorScheme: darkColorScheme,
+);
+
+const Color bluePurpleShade50 = Color.fromARGB(255, 247, 248, 254);
+const Color bluePurpleShade100 = Color(0xFFC5CAE9);
+const Color bluePurpleShade200 = Color(0xFF9FA8DA);
+const Color bluePurpleShade300 = Color(0xFF7986CB);
+const Color bluePurpleShade400 = Color(0xFF5C6BC0);
+const Color bluePurpleShade500 = Color(0xFF3F51B5);
+const Color bluePurpleShade600 = Color(0xFF3949AB);
+const Color bluePurpleShade700 = Color(0xFF303F9F);
+const Color bluePurpleShade800 = Color(0xFF283593);
+const Color bluePurpleShade900 = Color(0xFF1A237E);
+
+const ColorScheme darkColorScheme = ColorScheme.dark(
+  brightness: Brightness.dark,
+  // Primary color for main actions
+  primary: bluePurpleShade100,
+  // Text/Icon color on primary
+  onPrimary: Colors.white,
+  // Darker shade of primary
+  primaryContainer:  Color(0xFF512DA8),
+  // Text/Icon color on primary container
+  onPrimaryContainer: bluePurpleShade50,
+  // Secondary color for accents
+  secondary: AppColors.deepPurpleShade600,
+  // Text/Icon color on secondary
+  onSecondary: Colors.white,
+  //text/Icon color on secondary container
+  secondaryContainer: Color(0xFF673AB7),
+  // Error color
+  error: Colors.red,
+  // Text/Icon color on error
+  onError: Colors.black,
+  // Card backgrounds, Dialogs
+  surface: Color(0xFF121212),
+  // Text/Icon color on background
+  onSurface: Colors.white,
+  // Borders and outlines
+  outline: Color(0xFF616161),
+  // Shadows for cards, modals, etc.
+  shadow: Colors.black45,
+  // Used for lighter text background in dark mode
+  inverseSurface: Color(0xFFE0E0E0),
+  // Text/Icon on inverse surface
+  onInverseSurface: Colors.black,
+  // Tint for elevated surfaces
+  surfaceTint: Colors.purple,
 );
