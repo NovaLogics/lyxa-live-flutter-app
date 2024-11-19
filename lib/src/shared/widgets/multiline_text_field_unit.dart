4cc54f552297bd6da7beeb1f640947fe924ddbc8
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-19 21:36:30 +0530

    feat [ shared-widget ]: Update Widget [ MultilineTextFieldUnit ]

--- lib/src/shared/widgets/multiline_text_field_unit.dart
+++ lib/src/shared/widgets/multiline_text_field_unit.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lyxa_live/src/core/utils/constants/constants.dart';
+import 'package:lyxa_live/src/core/values/app_dimensions.dart';
 
 class MultilineTextFieldUnit extends StatelessWidget {
   final TextEditingController controller;
@@ -9,22 +11,32 @@ class MultilineTextFieldUnit extends StatelessWidget {
     super.key,
     required this.controller,
     required this.hintText,
-    this.maxLength = 100, 
+    this.maxLength = 100,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
-      maxLines: 5, 
-      maxLength: maxLength, // Sets max length
+      maxLines: 5,
+      maxLength: maxLength,
+      style: TextStyle(
+          color: Theme.of(context).colorScheme.inversePrimary,
+          fontFamily: FONT_RALEWAY,
+          fontWeight: FontWeight.w500,
+          fontSize: AppDimens.textSizeMedium),
       decoration: InputDecoration(
         labelText: hintText,
         hintText: hintText,
+        hintStyle: TextStyle(
+          color: Theme.of(context).colorScheme.onSecondary,
+          fontWeight: FontWeight.normal,
+          fontSize: AppDimens.textSizeRegular,
+        ),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(16), // Rounded corners
         ),
-        counterText: '', 
+        counterText: '',
       ),
     );
   }
