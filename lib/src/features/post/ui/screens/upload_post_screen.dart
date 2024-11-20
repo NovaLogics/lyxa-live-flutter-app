1d9631346d203117c354c536bf21ec086012f72d
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-20 18:47:50 +0530

    feat [ post ]: Impl Screen [ UploadPostScreen ] > Stage 2.1

--- lib/src/features/post/ui/screens/upload_post_screen.dart
+++ lib/src/features/post/ui/screens/upload_post_screen.dart
@@ -5,14 +5,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kDebugMode, kIsWeb;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:lyxa_live/src/core/di/service_locator.dart';
+import 'package:lyxa_live/src/core/styles/app_text_styles.dart';
+import 'package:lyxa_live/src/core/utils/constants/constants.dart';
+import 'package:lyxa_live/src/core/utils/helper/logger.dart';
+import 'package:lyxa_live/src/core/values/app_dimensions.dart';
+import 'package:lyxa_live/src/core/values/app_strings.dart';
 
 import 'package:lyxa_live/src/features/auth/domain/entities/app_user.dart';
-import 'package:lyxa_live/src/shared/widgets/text_field_unit.dart';
+import 'package:lyxa_live/src/features/auth/ui/components/gradient_button.dart';
+import 'package:lyxa_live/src/shared/widgets/gradient_background_unit.dart';
+import 'package:lyxa_live/src/shared/widgets/multiline_text_field_unit.dart';
+import 'package:lyxa_live/src/shared/widgets/responsive/scrollable_scaffold.dart';
 import 'package:lyxa_live/src/features/auth/cubits/auth_cubit.dart';
 import 'package:lyxa_live/src/features/post/domain/entities/post.dart';
 import 'package:lyxa_live/src/features/post/cubits/post_cubit.dart';
 import 'package:lyxa_live/src/features/post/cubits/post_state.dart';
-import 'package:lyxa_live/src/shared/widgets/responsive/constrained_scaffold.dart';
 
 class UploadPostScreen extends StatefulWidget {
   const UploadPostScreen({super.key});
@@ -27,7 +35,7 @@ class _UploadPostScreenState extends State<UploadPostScreen> {
   PlatformFile? imagePickedFile;
 
   // Web Image Pick
-  Uint8List? webImage;
+  Uint8List? pickedImage;
 
   //Bio Text Controller
   final textController = TextEditingController();
@@ -59,7 +67,7 @@ class _UploadPostScreenState extends State<UploadPostScreen> {
         imagePickedFile = result.files.first;
 
         if (kIsWeb) {
-          webImage = imagePickedFile!.bytes;
+          pickedImage = imagePickedFile!.bytes;
         }
       });
     }
@@ -124,9 +132,7 @@ class _UploadPostScreenState extends State<UploadPostScreen> {
   Widget build(BuildContext context) {
     return BlocConsumer<PostCubit, PostState>(
       builder: (context, state) {
-        if (kDebugMode) {
-          print(state);
-        }
+        Logger.logDebug(state.toString());
         // Loading or Uploading
         if (state is PostLoading || state is PostUploading) {
           return const Scaffold(
@@ -136,7 +142,14 @@ class _UploadPostScreenState extends State<UploadPostScreen> {
           );
         }
         // Build upload page
-        return _buildUploadPage();
+        return Scaffold(
+          body: Stack(
+            children: [
+              _buildBackground(),
+              _buildUploadPage(),
+            ],
+          ),
+        );
       },
       // Go to previous screen when upload is done & posts are loaded
       listener: (context, state) {
@@ -147,11 +160,60 @@ class _UploadPostScreenState extends State<UploadPostScreen> {
     );
   }
 
+  Widget _buildBackground() {
+    return RepaintBoundary(
+      child: getIt<GradientBackgroundUnit>(
+        param1: AppDimens.containerSize400,
+        param2: BackgroundStyle.home,
+      ),
+    );
+  }
+
+  Widget _buildPickImageButton() {
+    return Center(
+      child: GradientButton(
+        text: AppStrings.pickImage.toUpperCase(),
+        onPressed: pickImage,
+        textStyle: AppTextStyles.buttonTextPrimary.copyWith(
+          color: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        icon: Icon(Icons.filter,
+            color: Theme.of(context).colorScheme.inversePrimary),
+      ),
+    );
+  }
+
+  Widget _buildCaptionSection() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: AppDimens.size24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Caption',
+            style: AppTextStyles.subtitleSecondary.copyWith(
+              color: Theme.of(context).colorScheme.onPrimary,
+              fontWeight: FontWeight.bold,
+              shadows: [],
+            ),
+          ),
+          const SizedBox(height: AppDimens.size4),
+          MultilineTextFieldUnit(
+            controller: textController,
+            labelText: 'Caption area',
+            hintText: 'Add post caption here..',
+            maxLength: MAX_LENGTH_POST_FIELD,
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _buildUploadPage() {
-    return ConstrainedScaffold(
+    return ScrollableScaffold(
       // App Bar
       appBar: AppBar(
-        foregroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
         title: const Text("Create Post"),
         actions: [
           // Upload button
@@ -162,32 +224,29 @@ class _UploadPostScreenState extends State<UploadPostScreen> {
         ],
       ),
       // Body
-      body: Center(
-        child: Column(
-          children: [
-            // Image preview for web
-            if (kIsWeb && webImage != null) Image.memory(webImage!),
+      body: Column(
+        children: [
+          // Image preview for web
+          (pickedImage != null)
+              ? Image.memory(
+                  pickedImage!,
+                  width: AppDimens.imageSize180,
+                  height: AppDimens.imageSize180,
+                  fit: BoxFit.cover,
+                )
+              : Icon(
+                  Icons.image,
+                  size: AppDimens.imageSize180,
+                  color: Theme.of(context).colorScheme.outline,
+                ),
 
-            // Image preview for mobile
-            if (!kIsWeb && imagePickedFile != null)
-              Image.file(File(imagePickedFile!.path!)),
+          // Pick image button
+          _buildPickImageButton(),
+          const SizedBox(height: AppDimens.size28),
 
-            // Pick image button
-            MaterialButton(
-              onPressed: pickImage,
-              color: Colors.blueAccent,
-              child: const Text('Pick Image'),
-            ),
-
-            // Caption Text
-            TextFieldUnit(
-              controller: textController,
-              hintText: 'Caption',
-              obscureText: false,
-            prefixIcon: null,
-            ),
-          ],
-        ),
+          // Caption Text
+          _buildCaptionSection(),
+        ],
       ),
     );
   }
