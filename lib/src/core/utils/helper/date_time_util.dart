ea9da9b0b89feb5c9a52b49e53c9d1019a6bf90d
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-20 19:18:55 +0530

    feat [ core-util ]: Update DateTimeUtil > method datetimeAgo

--- lib/src/core/utils/helper/date_time_util.dart
+++ lib/src/core/utils/helper/date_time_util.dart
@@ -71,34 +71,40 @@ class DateTimeUtil {
     }
   }
 
-  static String datetimeAgo(DateTime dateTime) {
-    final Duration diff = DateTime.now().difference(dateTime);
+static String datetimeAgo(DateTime dateTime) {
+  final Duration diff = DateTime.now().difference(dateTime);
 
-    // Check for years
-    if (diff.inDays >= 365) {
-      final int years = (diff.inDays / 365).floor();
-      return '${years} year${years > 1 ? 's' : ''} ago';
-    }
-    // Check for weeks (more than 7 days but less than a year)
-    else if (diff.inDays > 7) {
-      return DateFormat('yyyy-MM-dd')
-          .format(dateTime); // If older than 7 days, return the date.
-    }
-    // Check for days
-    else if (diff.inDays >= 1) {
-      return '${diff.inDays} day${diff.inDays > 1 ? 's' : ''} ago';
-    }
-    // Check for hours
-    else if (diff.inHours >= 1) {
-      return '${diff.inHours} hour${diff.inHours > 1 ? 's' : ''} ago';
-    }
-    // Check for minutes
-    else if (diff.inMinutes >= 1) {
-      return '${diff.inMinutes} minute${diff.inMinutes > 1 ? 's' : ''} ago';
-    }
-    // Check for seconds
-    else {
-      return '${diff.inSeconds} second${diff.inSeconds > 1 ? 's' : ''} ago';
-    }
+  // Check for years
+  if (diff.inDays >= 365) {
+    final int years = (diff.inDays / 365).floor();
+    return '$years year${years > 1 ? 's' : ''} ago';
   }
+  // Check for months
+  else if (diff.inDays >= 30) {
+    final int months = (diff.inDays / 30).floor();
+    return '$months month${months > 1 ? 's' : ''} ago';
+  }
+  // Check for weeks
+  else if (diff.inDays >= 7) {
+    final int weeks = (diff.inDays / 7).floor();
+    return '$weeks week${weeks > 1 ? 's' : ''} ago';
+  }
+  // Check for days
+  else if (diff.inDays >= 1) {
+    return '${diff.inDays} day${diff.inDays > 1 ? 's' : ''} ago';
+  }
+  // Check for hours
+  else if (diff.inHours >= 1) {
+    return '${diff.inHours} hour${diff.inHours > 1 ? 's' : ''} ago';
+  }
+  // Check for minutes
+  else if (diff.inMinutes >= 1) {
+    return '${diff.inMinutes} minute${diff.inMinutes > 1 ? 's' : ''} ago';
+  }
+  // Check for seconds
+  else {
+    return '${diff.inSeconds} second${diff.inSeconds > 1 ? 's' : ''} ago';
+  }
+}
+
 }
