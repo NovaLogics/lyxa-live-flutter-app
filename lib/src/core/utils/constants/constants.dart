c5dc4f6d26fa59aa993c45c53cc9bc10f1ebbcf0
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-21 17:09:37 +0530

    feat [ core-constants ]: Update constant file structure

--- lib/src/core/utils/constants/constants.dart
+++ lib/src/core/utils/constants/constants.dart
@@ -1,30 +1,37 @@
 // ignore_for_file: constant_identifier_names
 
+// Data Storage Paths
 const String FIRESTORE_COLLECTION_USERS = 'users';
 const String FIRESTORE_COLLECTION_POSTS = 'posts';
 const String STORAGE_PATH_PROFILE_IMAGES = 'profile_images';
 const String STORAGE_PATH_POST_IMAGES = 'post_images';
 
-const String IMAGE_PATH_LYXA_BANNER = 'assets/images/lyxa_banner.png';
+const String HIVE_BOX_LYXA = 'lyxaAppBox';
 
-const String ICON_SETTINGS_STYLE_1 = 'assets/icons/ic_settings_style_1.svg';
-const String ICON_COMMENT_BORDER = 'assets/icons/ic_comment.svg';
-const String ICON_COMMENT_STYLE_1 = 'assets/icons/ic_comment_style_1.svg';
-const String ICON_HEART_FILLED = 'assets/icons/ic_heart_filled.svg';
-const String ICON_HEART_BORDER = 'assets/icons/ic_heart_border.svg';
+// Base image path
+const String _imagePath = 'assets/images';
+// Image Paths
+const String IMAGE_PATH_LYXA_BANNER = '$_imagePath/lyxa_banner.png';
 
+// Base icon path
+const String _iconPath = 'assets/icons';
+// Icon Paths
+const String ICON_HEART_FILLED = '$_iconPath/ic_heart_filled.svg';
+const String ICON_HEART_BORDER = '$_iconPath/ic_heart_border.svg';
+const String ICON_COMMENT_BORDER = '$_iconPath/ic_comment.svg';
+const String ICON_COMMENT_STYLE_1 = '$_iconPath/ic_comment_style_1.svg';
+const String ICON_SETTINGS_STYLE_1 = '$_iconPath/ic_settings_style_1.svg';
+
+// Fonts
 const String FONT_RALEWAY = 'Raleway';
 const String FONT_DYNALIGHT = 'Dynalight';
 const String FONT_MONTSERRAT = 'Montserrat';
 
-const String HIVE_BOX_LYXA = 'lyxaAppBox';
-
+// Text Field Limits
+const int MAX_LENGTH_POST_FIELD = 500;
 const int MAX_LENGTH_EMAIL_FIELD = 128;
 const int MAX_LENGTH_USERNAME_FIELD = 30;
 const int MAX_LENGTH_PASSWORD_FIELD = 15;
+const int MAX_LENGTH_COMMENTS_FIELD = 500;
 const int MAX_LENGTH_FULL_NAME_FIELD = 100;
 const int MAX_LENGTH_BIO_DESCRIPTION_FIELD = 300;
-const int MAX_LENGTH_COMMENTS_FIELD = 500;
-const int MAX_LENGTH_POST_FIELD = 500;
-
-
