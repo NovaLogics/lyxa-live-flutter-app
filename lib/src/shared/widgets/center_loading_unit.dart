b4f937b816a332fe5f9ee2e2bb93279f2062d905
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-22 00:44:22 +0530

    feat [ shared-widget ]: Update Widget [ CenterLoadingUnit ]

--- lib/src/shared/widgets/center_loading_unit.dart
+++ lib/src/shared/widgets/center_loading_unit.dart
@@ -6,33 +6,43 @@ import 'package:lyxa_live/src/core/resources/app_strings.dart';
 class CenterLoadingUnit extends StatelessWidget {
   final String message;
 
-  const CenterLoadingUnit({super.key, this.message = AppStrings.pleaseWait});
+  const CenterLoadingUnit({
+    super.key,
+    this.message = AppStrings.pleaseWait,
+  });
 
   @override
   Widget build(BuildContext context) {
+    // BACKGROUND
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface.withOpacity(0.5),
       body: Center(
+        // DISPLAY CARD
         child: Card(
           color: Theme.of(context).colorScheme.surface,
           shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(AppDimens.radiusMD12)),
+            borderRadius: BorderRadius.circular(AppDimens.radiusMD12),
+          ),
           elevation: AppDimens.elevationMD8,
           shadowColor: Theme.of(context).colorScheme.primary,
           child: Padding(
             padding: const EdgeInsets.all(AppDimens.size56),
-            child: Column(
+            child:
+                // BODY
+                Column(
               mainAxisSize: MainAxisSize.min,
               children: [
+                // LOADING INDICATOR
                 CircularProgressIndicator(
                   color: Theme.of(context).colorScheme.onSecondary,
-                ), // Loading indicator
+                ),
                 const SizedBox(height: AppDimens.size24),
+                // DISPLAY TEXT
                 Text(
                   message,
                   style: TextStyle(
-                    fontSize: 20,
-                    letterSpacing: 1.1,
+                    fontSize: AppDimens.fontSizeXL20,
+                    letterSpacing: AppDimens.letterSpacingPT11,
                     fontFamily: FONT_RALEWAY,
                     fontWeight: FontWeight.bold,
                     color: Theme.of(context).colorScheme.onSecondary,
