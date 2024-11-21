318f0801cbdf7a63c11af7327d8a4bc3076a8404
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-21 17:21:05 +0530

    feat [ core-constants ]: Update constant file structure - final

--- lib/src/core/utils/constants/constants.dart
+++ lib/src/core/utils/constants/constants.dart
@@ -1,32 +1,32 @@
 // ignore_for_file: constant_identifier_names
 
-// Base res paths
-const String _imagePath = 'assets/images';
-const String _iconPath = 'assets/icons';
+// BASE RESOURCE PATHS
+const String _IMAGE_PATH = 'assets/images';
+const String _ICON_PATH = 'assets/icons';
 
-// Data Storage Paths
+// DATA STORAGE PATHS
 const String HIVE_BOX_LYXA = 'lyxaAppBox';
 const String FIRESTORE_COLLECTION_USERS = 'users';
 const String FIRESTORE_COLLECTION_POSTS = 'posts';
 const String STORAGE_PATH_PROFILE_IMAGES = 'profile_images';
 const String STORAGE_PATH_POST_IMAGES = 'post_images';
 
-// Image Paths
-const String IMAGE_LYXA_BANNER = '$_imagePath/lyxa_banner.png';
+// IMAGE PATHS
+const String IMAGE_LYXA_BANNER = '$_IMAGE_PATH/lyxa_banner.png';
 
-// Icon Paths
-const String ICON_HEART_FILLED = '$_iconPath/ic_heart_filled.svg';
-const String ICON_HEART_BORDER = '$_iconPath/ic_heart_border.svg';
-const String ICON_COMMENT_BORDER = '$_iconPath/ic_comment.svg';
-const String ICON_COMMENT_STYLE_1 = '$_iconPath/ic_comment_style_1.svg';
-const String ICON_SETTINGS_STYLE_1 = '$_iconPath/ic_settings_style_1.svg';
+// ICON PATHS
+const String ICON_HEART_FILLED = '$_ICON_PATH/ic_heart_filled.svg';
+const String ICON_HEART_BORDER = '$_ICON_PATH/ic_heart_border.svg';
+const String ICON_COMMENT_BORDER = '$_ICON_PATH/ic_comment.svg';
+const String ICON_COMMENT_STYLE_1 = '$_ICON_PATH/ic_comment_style_1.svg';
+const String ICON_SETTINGS_STYLE_1 = '$_ICON_PATH/ic_settings_style_1.svg';
 
-// Fonts
+// APP FONTS
 const String FONT_RALEWAY = 'Raleway';
 const String FONT_DYNALIGHT = 'Dynalight';
 const String FONT_MONTSERRAT = 'Montserrat';
 
-// Text Field Limits
+// TEXT FIELD LIMITS
 const int MAX_LENGTH_POST_FIELD = 500;
 const int MAX_LENGTH_EMAIL_FIELD = 128;
 const int MAX_LENGTH_USERNAME_FIELD = 30;
