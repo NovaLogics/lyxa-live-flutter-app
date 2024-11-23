4a20e4ce49fca2107120c00910eae2a178a2803d
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-24 01:57:40 +0530

    refactor [ auth ] : Update [ AuthCubit ] -> Stage 4 (v1): Improving the internal structure

--- lib/src/features/auth/cubits/auth_cubit.dart
+++ lib/src/features/auth/cubits/auth_cubit.dart
@@ -1,16 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lyxa_live/src/core/resources/app_strings.dart';
 import 'package:lyxa_live/src/core/utils/hive_helper.dart';
 import 'package:lyxa_live/src/core/utils/logger.dart';
 import 'package:lyxa_live/src/features/auth/domain/entities/app_user.dart';
 import 'package:lyxa_live/src/features/auth/domain/repositories/auth_repository.dart';
 import 'package:lyxa_live/src/features/auth/cubits/auth_state.dart';
+import 'package:lyxa_live/src/shared/entities/result.dart';
+import 'package:lyxa_live/src/shared/handlers/errors/cubits/error_cubit.dart';
+import 'package:lyxa_live/src/shared/handlers/errors/utils/error_handler.dart';
+import 'package:lyxa_live/src/shared/handlers/errors/utils/error_messages.dart';
 import 'package:lyxa_live/src/shared/handlers/loading/cubits/loading_cubit.dart';
 
 /// AuthCubit: Handles authentication state management
 /// ->
 class AuthCubit extends Cubit<AuthState> {
   final AuthRepository _authRepository;
-  AppUser? _currentUser;
+  AppUser _currentUser = AppUser.getDefaultGuestUser();
 
   AuthCubit({required AuthRepository authRepository})
       : _authRepository = authRepository,
@@ -32,26 +37,47 @@ class AuthCubit extends Cubit<AuthState> {
   AppUser? get currentUser => _currentUser;
 
   /// Logs in with email and password.
-  Future<void> login(String email, String password) async {
-    try {
-      //emit(AuthLoading());
-      LoadingCubit.showLoading(message: 'Loading data...');
+  Future<void> login(
+    String email,
+    String password,
+  ) async {
+    LoadingCubit.showLoading(message: AppStrings.authenticatingPleaseWait);
 
-      final user = await _authRepository.loginWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+    final result = await _authRepository.loginWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
 
-      if (user != null) {
-        _currentUser = user;
-        emit(Authenticated(user));
-      } else {
-        emit(Unauthenticated());
-      }
-    } catch (error) {
-      _handleAuthError(error);
-    }
     LoadingCubit.hideLoading();
+
+    switch (result.status) {
+      case Status.success:
+        if (result.isDataNotEmpty()) {
+          _currentUser = result.data as AppUser;
+
+          emit(Authenticated(_currentUser));
+        } else {
+          emit(Unauthenticated());
+        }
+        break;
+
+      case Status.errorMessage:
+        emit(AuthError(getError(result.errorMessage)));
+        break;
+
+      case Status.error:
+        ErrorHandler.handleError(
+          result.error,
+          stackTrace: null,
+          onRetry: () {
+            ErrorAlertCubit.hideErrorMessage();
+          },
+        );
+        break;
+
+      case Status.loading:
+        break;
+    }
   }
 
   /// Registers a new user with email and password.
@@ -79,7 +105,7 @@ class AuthCubit extends Cubit<AuthState> {
   /// Logs out the current user.
   Future<void> logout() async {
     await _authRepository.logOut();
-    _currentUser = null;
+    _currentUser = AppUser.getDefaultGuestUser();
     emit(Unauthenticated());
   }
 
@@ -100,5 +126,13 @@ class AuthCubit extends Cubit<AuthState> {
     emit(AuthError(error.toString().replaceFirst('Exception: ', '')));
     emit(Unauthenticated());
     Logger.logError(error.toString());
+  }
+
+  String getError(String? message) {
+    if (message != null && message.isNotEmpty) {
+      return message.toString().replaceFirst('Exception: ', '');
+    } else {
+      return ErrorMessages.authenticationError;
+    }
   }
 }
