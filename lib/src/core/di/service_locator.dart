aeac35521fbc50e5d07f4beb8a29c267730102ba
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-23 18:29:13 +0530

    feat [ core/di ] :  Add Instance [ ErrorAlertCubit ]

--- lib/src/core/di/service_locator.dart
+++ lib/src/core/di/service_locator.dart
@@ -5,6 +5,7 @@ import 'package:lyxa_live/src/features/post/data/firebase_post_repository.dart';
 import 'package:lyxa_live/src/features/profile/data/firebase_profile_repository.dart';
 import 'package:lyxa_live/src/features/search/data/firebase_search_repository.dart';
 import 'package:lyxa_live/src/features/storage/data/firebase_storage_repository.dart';
+import 'package:lyxa_live/src/shared/event_handlers/errors/cubits/error_cubit.dart';
 import 'package:lyxa_live/src/shared/event_handlers/loading/cubits/loading_cubit.dart';
 import 'package:lyxa_live/src/shared/event_handlers/loading/widgets/center_loading_unit.dart';
 import 'package:lyxa_live/src/shared/widgets/gradient_background_unit.dart';
@@ -12,20 +13,33 @@ import 'package:lyxa_live/src/shared/widgets/gradient_background_unit.dart';
 final GetIt getIt = GetIt.instance;
 
 void setupServiceLocator() {
-
   // Register repositories as singletons
-  getIt.registerLazySingleton<FirebaseAuthRepository>(() => FirebaseAuthRepository());
-  getIt.registerLazySingleton<FirebaseProfileRepository>(() => FirebaseProfileRepository());
-  getIt.registerLazySingleton<FirebaseStorageRepository>(() => FirebaseStorageRepository());
-  getIt.registerLazySingleton<FirebasePostRepository>(() => FirebasePostRepository());
-  getIt.registerLazySingleton<FirebaseSearchRepository>(() => FirebaseSearchRepository());
+  getIt.registerLazySingleton<FirebaseAuthRepository>(
+      () => FirebaseAuthRepository());
 
-   getIt.registerSingleton<LoadingCubit>(LoadingCubit());
-  
+  getIt.registerLazySingleton<FirebaseProfileRepository>(
+      () => FirebaseProfileRepository());
+
+  getIt.registerLazySingleton<FirebaseStorageRepository>(
+      () => FirebaseStorageRepository());
+
+  getIt.registerLazySingleton<FirebasePostRepository>(
+      () => FirebasePostRepository());
+
+  getIt.registerLazySingleton<FirebaseSearchRepository>(
+      () => FirebaseSearchRepository());
+
+  getIt.registerSingleton<LoadingCubit>(LoadingCubit());
+
+  getIt.registerSingleton<ErrorAlertCubit>(ErrorAlertCubit());
+
   getIt.registerFactory(() => HiveHelper());
 
   getIt.registerFactoryParam<GradientBackgroundUnit, double, BackgroundStyle>(
-    (widthSize, style) => GradientBackgroundUnit(width: widthSize, style: style,),
+    (widthSize, style) => GradientBackgroundUnit(
+      width: widthSize,
+      style: style,
+    ),
   );
 
   getIt.registerFactoryParam<CenterLoadingUnit, String, void>(
