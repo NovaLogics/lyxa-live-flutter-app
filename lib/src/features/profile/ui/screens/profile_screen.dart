d735f3dde11fcc165755a67537348a724b79630f
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-23 19:39:21 +0530

    feat [ minor changes ]: Clean code

--- lib/src/features/profile/ui/screens/profile_screen.dart
+++ lib/src/features/profile/ui/screens/profile_screen.dart
@@ -51,7 +51,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
           return _buildProfileContent(context, state.profileUser);
         } else if (state is ProfileLoading) {
           LoadingCubit.showLoading();
-          return const SizedBox();
+          return Scaffold(
+            backgroundColor: Theme.of(context).colorScheme.surface,
+          );
         } else {
           LoadingCubit.hideLoading();
           return const Scaffold(
@@ -117,7 +119,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
       });
     });
   }
-
 
   Widget _buildProfileContent(BuildContext context, ProfileUser user) {
     final isOwnProfile = (widget.displayUserId == _currentAppUser.uid);
--- lib/src/shared/event_handlers/loading/cubits/loading_cubit.dart
+++ lib/src/shared/event_handlers/loading/cubits/loading_cubit.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lyxa_live/src/core/di/service_locator.dart';
 import 'package:lyxa_live/src/shared/event_handlers/loading/cubits/loading_state.dart';
