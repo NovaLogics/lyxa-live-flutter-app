3c3b47c77b718a37fd8c11622a8a15cb42ad668b
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-23 18:28:54 +0530

    feat [ event_handlers/error ] :  Update Cubit [ ErrorAlertCubit ]

--- lib/src/shared/event_handlers/errors/cubits/error_cubit.dart
+++ lib/src/shared/event_handlers/errors/cubits/error_cubit.dart
@@ -1,17 +1,44 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lyxa_live/src/shared/event_handlers/errors/utils/error_messages.dart';
+
 import 'package:lyxa_live/src/shared/event_handlers/errors/utils/error_type.dart';
 import 'error_state.dart';
 
-class ErrorCubit extends Cubit<ErrorState> {
-  ErrorCubit() : super(ErrorHidden());
+class ErrorAlertCubit extends Cubit<ErrorAlertState> {
+  ErrorAlertCubit()
+      : super(ErrorAlertState(errorType: ErrorType.unknown, onRetry: () {}));
 
-  /// Display an error with an optional custom message
-  void showError(ErrorType errorType, {String? customMessage}) {
-    emit(ErrorVisible(errorType: errorType, customMessage: customMessage));
+  void showError({
+    required ErrorType errorType,
+    String? customMessage,
+    required VoidCallback onRetry,
+  }) {
+    emit(state.copyWith(
+      isVisible: true,
+      errorMessage: customMessage ?? _getErrorMessage(errorType),
+      errorType: errorType,
+      onRetry: onRetry,
+    ));
   }
 
-  /// Hide the error
   void hideError() {
-    emit(ErrorHidden());
+    emit(state.copyWith(isVisible: false));
+  }
+
+  String _getErrorMessage(ErrorType errorType) {
+    switch (errorType) {
+      case ErrorType.networkError:
+        return ErrorMessages.networkError;
+      case ErrorType.timeoutError:
+        return ErrorMessages.timeoutError;
+      case ErrorType.authenticationError:
+        return ErrorMessages.authenticationError;
+      case ErrorType.permissionDenied:
+        return ErrorMessages.permissionDeniedError;
+      case ErrorType.unknown:
+      default:
+        return ErrorMessages.unknownError;
+    }
   }
 }
