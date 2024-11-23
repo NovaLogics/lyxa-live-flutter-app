fdf06a05aabc578c660c73dbbc468e20722cae42
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-23 17:13:10 +0530

    feat [ event_handlers/loading ] :  Update Cubit [ LoadingCubit ]

--- lib/src/shared/event_handlers/loading/cubits/loading_cubit.dart
+++ lib/src/shared/event_handlers/loading/cubits/loading_cubit.dart
@@ -1,8 +1,14 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lyxa_live/src/shared/event_handlers/loading/cubits/loading_state.dart';
 
-class LoadingCubit extends Cubit<bool> {
-  LoadingCubit() : super(false);
+class LoadingCubit extends Cubit<LoadingState> {
+  LoadingCubit() : super(LoadingState());
 
-  void show() => emit(true);
-  void hide() => emit(false);
+  void show({String? message}) {
+    emit(state.copyWith(isVisible: true, message: message));
+  }
+
+  void hide() {
+    emit(state.copyWith(isVisible: false));
+  }
 }
