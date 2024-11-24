8fec5753da7b4f251f4fff1646a82a455181d104
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-24 17:19:56 +0530

    refactor [ post ] : Stage 4 (v1) : Update [ PostRepository ] -> Improving the internal structure

--- lib/src/features/post/domain/repositories/post_repository.dart
+++ lib/src/features/post/domain/repositories/post_repository.dart
@@ -1,12 +1,34 @@
 import 'package:lyxa_live/src/features/post/domain/entities/comment.dart';
 import 'package:lyxa_live/src/features/post/domain/entities/post.dart';
+import 'package:lyxa_live/src/shared/entities/result.dart';
 
 abstract class PostRepository {
-  Future<List<Post>> fetchAllPosts();
-  Future<void> createPost(Post post);
-  Future<void> deletePost(String postId);
-  Future<List<Post>> fetchPostsByUserId(String userId);
-  Future<void> toggleLikePost(String postId, String userId);
-  Future<void> addComment(String postId, Comment comment);
-  Future<void> deleteComment(String postId, String commentId);
+  Future<Result<List<Post>>> fetchAllPosts();
+
+  Future<void> createPost(
+    Post post,
+  );
+
+  Future<void> deletePost(
+    String postId,
+  );
+
+  Future<Result<List<Post>>> fetchPostsByUserId(
+    String userId,
+  );
+
+  Future<void> toggleLikePost(
+    String postId,
+    String userId,
+  );
+
+  Future<void> addComment(
+    String postId,
+    Comment comment,
+  );
+
+  Future<void> deleteComment(
+    String postId,
+    String commentId,
+  );
 }
