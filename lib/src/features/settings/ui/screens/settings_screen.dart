d0cc3f3f27ff69609485f642c21e1d8c6d5cdd0d
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-25 21:41:09 +0530

    refactor [ settings ] :  Stage 4 (v1) : Update Feature [ Full Feature ]

--- lib/src/features/settings/ui/screens/settings_screen.dart
+++ lib/src/features/settings/ui/screens/settings_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lyxa_live/src/core/di/service_locator.dart';
 import 'package:lyxa_live/src/core/styles/app_text_styles.dart';
 import 'package:lyxa_live/src/core/resources/app_dimensions.dart';
 import 'package:lyxa_live/src/core/resources/app_strings.dart';
@@ -12,34 +12,47 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final themeCubit = context.watch<ThemeCubit>();
-
-    bool isDarkMode = themeCubit.isDarkMode;
-
     return ConstrainedScaffold(
-      appBar: AppBar(
-        title: const Text(AppStrings.settings),
+      appBar: _buildAppBar(),
+      body: _buildSettingsBody(context),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: const Text(AppStrings.settings),
+    );
+  }
+
+  Widget _buildSettingsBody(BuildContext context) {
+    final ThemeCubit themeCubit = getIt<ThemeCubit>();
+    final bool isDarkModeEnabled = themeCubit.isDarkMode;
+
+    return Column(
+      children: [
+        _buildDarkModeSwitch(context, themeCubit, isDarkModeEnabled),
+      ],
+    );
+  }
+
+  Widget _buildDarkModeSwitch(
+      BuildContext context, ThemeCubit themeCubit, bool isDarkModeEnabled) {
+    return ListTile(
+      title: Text(
+        AppStrings.darkMode,
+        style: AppTextStyles.textStylePost.copyWith(
+          color: Theme.of(context).colorScheme.inversePrimary,
+          fontWeight: FontWeight.bold,
+          fontSize: AppDimens.fontSizeLG18,
+        ),
       ),
-      body: Column(
-        children: [
-          ListTile(
-            title: Text(
-              AppStrings.darkMode,
-              style: AppTextStyles.textStylePost.copyWith(
-                color: Theme.of(context).colorScheme.inversePrimary,
-                fontWeight: FontWeight.bold,
-                fontSize: AppDimens.fontSizeLG18,
-              ),
-            ),
-            trailing: CupertinoSwitch(
-              value: isDarkMode,
-              onChanged: (value) {
-                themeCubit.toggleTheme();
-              },
-            ),
-          )
-        ],
+      trailing: CupertinoSwitch(
+        value: isDarkModeEnabled,
+        onChanged: (value) {
+          themeCubit.toggleTheme();
+        },
       ),
     );
   }
 }
+
