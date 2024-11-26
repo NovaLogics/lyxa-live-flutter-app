9c65bdef4f46c296702a0480712f48491f682efa
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-27 01:41:18 +0530

    refactor [ shared/loading ] : Stage 4 (v7) : Update Widget Structure

--- lib/src/shared/handlers/loading/widgets/loading_unit.dart
+++ lib/src/shared/handlers/loading/widgets/loading_unit.dart
@@ -19,54 +19,82 @@ class LoadingUnit extends StatelessWidget {
     return BlocBuilder<LoadingCubit, LoadingState>(
       builder: (context, state) {
         return state.isVisible
-            ? Scaffold(
-                backgroundColor:
-                    Theme.of(context).colorScheme.surface.withOpacity(0.6),
-                body: Center(
-                  child: Card(
-                    color: Theme.of(context).colorScheme.surface,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(AppDimens.radiusMD12),
-                    ),
-                    elevation: AppDimens.elevationMD8,
-                    shadowColor: Theme.of(context).colorScheme.primary,
-                    child: Padding(
-                      padding: const EdgeInsets.all(AppDimens.size56),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          CircularProgressIndicator(
-                            color: Theme.of(context).colorScheme.onSecondary,
-                          ),
-                          const SizedBox(height: AppDimens.size24),
-                          SizedBox(
-                            width: AppDimens.size240,
-                            child: Text(
-                              state.message,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: AppDimens.fontSizeXL20,
-                                letterSpacing: AppDimens.letterSpacingPT11,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: FONT_RALEWAY,
-                                color:
-                                    Theme.of(context).colorScheme.onSecondary,
-                              ),
-                              maxLines: null, // Allows unlimited lines
-                              overflow: TextOverflow
-                                  .visible, // Ensures the text remains visible
-                              softWrap: true, // Wraps the text within the width
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            : const SizedBox
-                .shrink(); // Return an empty widget when not visible
+            ? _buildLoadingScaffold(context, state.message)
+            : const SizedBox.shrink();
       },
+    );
+  }
+
+  Widget _buildLoadingScaffold(BuildContext context, String message) {
+    return Scaffold(
+      backgroundColor: _getBackgroundColor(context),
+      body: Center(
+        child: _buildLoadingCard(context, message),
+      ),
+    );
+  }
+
+  Color _getBackgroundColor(BuildContext context) {
+    return Theme.of(context).colorScheme.surface.withOpacity(0.7);
+  }
+
+  Widget _buildLoadingCard(BuildContext context, String message) {
+    return Card(
+      color: Theme.of(context).colorScheme.surface,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(AppDimens.radiusMD12),
+      ),
+      elevation: AppDimens.elevationMD8,
+      shadowColor: Theme.of(context).colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: AppDimens.size44,
+          vertical: AppDimens.size56,
+        ),
+        child: _buildLoadingContent(context, message),
+      ),
+    );
+  }
+
+  // Builds the content of the loading card
+  Widget _buildLoadingContent(BuildContext context, String message) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        _buildLoadingIndicator(context),
+        const SizedBox(height: AppDimens.size36),
+        _buildLoadingMessage(context, message),
+      ],
+    );
+  }
+
+  Widget _buildLoadingIndicator(BuildContext context) {
+    return CircularProgressIndicator(
+      color: Theme.of(context).colorScheme.onSecondary,
+    );
+  }
+
+  Widget _buildLoadingMessage(BuildContext context, String message) {
+    return SizedBox(
+      width: AppDimens.size220,
+      child: Text(
+        message,
+        textAlign: TextAlign.center,
+        style: _getMessageTextStyle(context),
+        maxLines: 6,
+        overflow: TextOverflow.visible,
+        softWrap: true,
+      ),
+    );
+  }
+
+  TextStyle _getMessageTextStyle(BuildContext context) {
+    return TextStyle(
+      fontSize: AppDimens.fontSizeXL20,
+      letterSpacing: AppDimens.letterSpacingPT07,
+      fontWeight: FontWeight.w600,
+      fontFamily: FONT_RALEWAY,
+      color: Theme.of(context).colorScheme.onTertiary,
     );
   }
 }
