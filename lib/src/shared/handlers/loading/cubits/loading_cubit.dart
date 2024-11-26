6705d2df9727c469ebb9a8c6b4c373f7562ea32d
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-27 01:27:47 +0530

    refactor [ shared/loading ] : Stage 4 (v5) : Update Widget Structure

--- lib/src/shared/handlers/loading/cubits/loading_cubit.dart
+++ lib/src/shared/handlers/loading/cubits/loading_cubit.dart
@@ -1,27 +1,52 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lyxa_live/src/core/di/service_locator.dart';
 import 'package:lyxa_live/src/shared/handlers/loading/cubits/loading_state.dart';
 
 class LoadingCubit extends Cubit<LoadingState> {
-
   LoadingCubit() : super(LoadingState());
 
-  void show({String? message}) {
-    emit(state.copyWith(isVisible: true, message: message));
-  }
+  Timer? _loadingScreenhideTimer;
+  Timer? _visibilityDelayTimer;
 
-  void hide() {
-    if(state.isVisible) {
-      emit(state.copyWith(isVisible: false));
+  void show({String? message, Duration? timeout}) {
+    _loadingScreenhideTimer?.cancel();
+    _visibilityDelayTimer?.cancel();
+
+    emit(state.copyWith(isVisible: true, message: message));
+
+    if (timeout != null) {
+      _loadingScreenhideTimer = Timer(timeout, () {
+        hide(); 
+      });
     }
   }
 
-  // Add static shortcut methods for convenience
-  static void showLoading({String? message}) {
-    getIt<LoadingCubit>().show(message: message);
+  void hide() {
+    if (state.isVisible) {
+      _loadingScreenhideTimer?.cancel();
+
+      _visibilityDelayTimer = Timer(const Duration(seconds: 2), () {
+        emit(state.copyWith(isVisible: false));
+      });
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _loadingScreenhideTimer?.cancel();
+    _visibilityDelayTimer?.cancel();
+    return super.close();
+  }
+
+
+  static void showLoading({String? message, Duration? timeout}) {
+    getIt<LoadingCubit>().show(message: message, timeout: timeout);
   }
 
   static void hideLoading() {
     getIt<LoadingCubit>().hide();
   }
 }
+
