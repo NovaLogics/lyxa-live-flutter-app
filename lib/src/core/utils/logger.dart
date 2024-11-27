223dd493cdc6b59947cf500d7bfd0d078e2fb8d8
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-28 05:18:17 +0530

    fix [ project ] :  Stage 5 (v19) : QA bug fixes

--- lib/src/core/utils/logger.dart
+++ lib/src/core/utils/logger.dart
@@ -3,34 +3,45 @@ import 'package:intl/intl.dart';
 
 class Logger {
   static const String debugTag = 'Logger';
-  static void log(String message,
-      {String tag = "Logger", String level = "INFO"}) {
+  static const String levelInfo = 'INFO';
+  static const String levelDebug = 'DEBUG';
+  static const String levelWarning = 'WARNING';
+  static const String levelError = 'ERROR';
+  static const String _dateTimeFormat = 'yyyy-MM-dd HH:mm:ss';
+
+  static void log(
+    String message, {
+    String tag = debugTag,
+    String level = levelInfo,
+  }) {
     String formattedMessage = _formatMessage(tag, message, level);
     if (kDebugMode) {
       print(formattedMessage);
     }
   }
 
-  // Specific log levels
   static void logDebug(String message, {String tag = debugTag}) {
-    log(message, tag: tag, level: "DEBUG");
+    log(message, tag: tag, level: levelDebug);
   }
 
   static void logInfo(String message, {String tag = debugTag}) {
-    log(message, tag: tag, level: "INFO");
+    log(message, tag: tag, level: levelInfo);
   }
 
   static void logWarning(String message, {String tag = debugTag}) {
-    log(message, tag: tag, level: "WARNING");
+    log(message, tag: tag, level: levelWarning);
   }
 
   static void logError(String message, {String tag = debugTag}) {
-    log(message, tag: tag, level: "ERROR");
+    log(message, tag: tag, level: levelError);
   }
 
-  // Helper method to format the message
-  static String _formatMessage(String tag, String message, String level) {
-    String timeStamp = DateFormat("yyyy-MM-dd HH:mm:ss").format(DateTime.now());
-    return "[$debugTag | $timeStamp] [$level] [$tag] $message";
+  static String _formatMessage(
+    String tag,
+    String message,
+    String level,
+  ) {
+    String timeStamp = DateFormat(_dateTimeFormat).format(DateTime.now());
+    return '[$debugTag | $timeStamp] [$level] [$tag] $message';
   }
 }
