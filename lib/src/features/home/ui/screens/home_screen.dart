3b83e7593358914fbb579c66d71f90f19487dd74
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-04 17:42:26 +0530

    feat [ home ]: Update Screen > HomeScreen

--- lib/src/features/home/ui/screens/home_screen.dart
+++ lib/src/features/home/ui/screens/home_screen.dart
@@ -6,15 +6,17 @@ import 'package:lyxa_live/src/core/resources/app_dimensions.dart';
 import 'package:lyxa_live/src/core/resources/app_strings.dart';
 import 'package:lyxa_live/src/core/styles/app_styles.dart';
 import 'package:lyxa_live/src/core/utils/logger.dart';
+import 'package:lyxa_live/src/features/home/cubits/home_cubit.dart';
+import 'package:lyxa_live/src/features/home/cubits/home_state.dart';
 import 'package:lyxa_live/src/features/home/ui/components/drawer_unit.dart';
 import 'package:lyxa_live/src/features/home/ui/components/refresh_button_unit.dart';
 import 'package:lyxa_live/src/features/post/domain/entities/post_entity.dart';
 import 'package:lyxa_live/src/features/profile/domain/entities/profile_user.dart';
+import 'package:lyxa_live/src/shared/handlers/errors/utils/error_handler.dart';
 import 'package:lyxa_live/src/shared/widgets/post_tile/post_tile_unit.dart';
-import 'package:lyxa_live/src/features/post/cubits/post_cubit.dart';
-import 'package:lyxa_live/src/features/post/cubits/post_state.dart';
 import 'package:lyxa_live/src/features/post/ui/screens/upload_post_screen.dart';
 import 'package:lyxa_live/src/shared/widgets/responsive/constrained_scaffold.dart';
+import 'package:lyxa_live/src/shared/widgets/toast_messenger_unit.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -24,13 +26,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late final PostCubit _postCubit;
+  static const String debugTag = 'HomeScreen';
+  late final HomeCubit _homeCubit;
   ProfileUser _currentUser = ProfileUser.getGuestUser();
 
   @override
   void initState() {
     super.initState();
-    _postCubit = getIt<PostCubit>();
+    _homeCubit = getIt<HomeCubit>();
     _initScreen();
   }
 
@@ -39,37 +42,55 @@ class _HomeScreenState extends State<HomeScreen> {
     return ConstrainedScaffold(
       appBar: _buildAppBar(context),
       drawer: _buildAppDrawer(),
-      body: BlocBuilder<PostCubit, PostState>(
+      body: BlocBuilder<HomeCubit, HomeState>(
         builder: (context, state) {
-          if (state is PostLoaded) {
+          if (state is HomeLoaded) {
+            if (state.errorMessage != null) {
+              _handleErrorToast(state.errorMessage!);
+            }
             return _buildPostList(state.posts);
-          } else if (state is PostError) {
+          } else if (state is HomeError) {
+            _handleExceptionMessage(error: state.error, message: state.message);
             return _buildDisplayMsgScreen(message: state.message);
-          } else {
-            return _buildDisplayMsgScreen();
           }
+          return _buildDisplayMsgScreen();
         },
       ),
     );
   }
 
-  void _fetchAllPosts() {
-    _postCubit.getAllPosts();
-  }
-
-  void _deletePost(PostEntity post) {
-    _postCubit.deletePost(post: post);
-    //_fetchAllPosts();
-  }
-
   void _initScreen() async {
-    final profileUser = await _postCubit.getCurrentUser();
+    final profileUser = await _homeCubit.getCurrentUser();
 
     setState(() {
       _currentUser = profileUser;
     });
-    _fetchAllPosts();
+    _homeCubit.getAllPostsfromServer();
   }
+
+  void _deletePost(PostEntity post) {
+    _homeCubit.deletePost(post: post);
+  }
+
+  void _handleErrorToast(String message) {
+    _hideKeyboard();
+    ToastMessengerUnit.showErrorToast(
+      context: context,
+      message: message,
+    );
+  }
+
+  void _handleExceptionMessage({Object? error, String? message}) {
+    _hideKeyboard();
+    ErrorHandler.handleError(
+      error,
+      tag: debugTag,
+      customMessage: message,
+      onRetry: () {},
+    );
+  }
+
+  void _hideKeyboard() => FocusScope.of(context).unfocus();
 
   void _navigateToUploadPostScreen() {
     Navigator.push(
@@ -105,7 +126,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       actions: [
         RefreshButtonUnit(
-          onRefresh: _fetchAllPosts,
+          onRefresh: _homeCubit.getAllPostsfromServer,
         ),
         Padding(
           padding: const EdgeInsets.only(right: AppDimens.size2),
@@ -119,15 +140,15 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _buildPostList(List<PostEntity> posts) {
-    return (posts.isEmpty)
+  Widget _buildPostList(List<PostEntity> postList) {
+    return (postList.isEmpty)
         ? _buildDisplayMsgScreen(
             message: AppStrings.noPostAvailableError,
           )
         : ListView.builder(
-            itemCount: posts.length,
+            itemCount: postList.length,
             itemBuilder: (context, index) {
-              final post = posts[index];
+              final post = postList[index];
               return PostTileUnit(
                 post: post,
                 currentUser: _currentUser,
