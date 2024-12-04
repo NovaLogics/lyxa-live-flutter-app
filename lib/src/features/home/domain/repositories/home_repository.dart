a24747839e7eec527787abab2d1b9a62c9484179
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-04 14:30:43 +0530

    feat [ home ]: Add Repo > HomeRepository

---
+++ lib/src/features/home/domain/repositories/home_repository.dart
@@ -0,0 +1,9 @@
+import 'package:lyxa_live/src/features/post/domain/entities/post_entity.dart';
+import 'package:lyxa_live/src/features/profile/domain/entities/profile_user.dart';
+import 'package:lyxa_live/src/shared/entities/result/result.dart';
+
+abstract class HomeRepository {
+  Future<Result<ProfileUser>> getCurrentAppUser();
+
+  Future<Result<List<PostEntity>>> getAllPosts();
+}
