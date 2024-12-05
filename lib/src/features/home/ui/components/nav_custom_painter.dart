9175f128274146df39569add1af555529fb70795
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-05 13:58:19 +0530

    feat [ NavBar ]: Add NavBar v5

--- lib/src/features/home/ui/components/nav_custom_painter.dart
+++ lib/src/features/home/ui/components/nav_custom_painter.dart
@@ -11,16 +11,16 @@ class NavCustomPainter extends CustomPainter {
       ..style = PaintingStyle.fill;
 
     Path path = Path();
-    path.moveTo(0, 5); // Start
+    path.moveTo(0, 2); // Start
     path.quadraticBezierTo(size.width * 0.20, 0, size.width * 0.35, 0);
     path.quadraticBezierTo(size.width * 0.40, 0, size.width * 0.40, 20);
     path.arcToPoint(Offset(size.width * 0.60, 20),
-        radius: const Radius.circular(20.0), clockwise: false);
+        radius: const Radius.circular(30.0), clockwise: false);
     path.quadraticBezierTo(size.width * 0.60, 0, size.width * 0.65, 0);
-    path.quadraticBezierTo(size.width * 0.80, 0, size.width, 20);
+    path.quadraticBezierTo(size.width * 0.80, 0, size.width, 2);
     path.lineTo(size.width, size.height);
     path.lineTo(0, size.height);
-    path.lineTo(0, 35);
+    path.lineTo(0, 20);
     path.close();
     canvas.drawShadow(path, Theme.of(context).colorScheme.onPrimary, 16, false);
     canvas.drawPath(path, paint);
