afb3f83cdd7ae183c6171c712768f14bef09691a
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-06 15:48:36 +0530

    refactor [ post ]: Stage 7(v76)> Update Structure [ Full Feature ]

---
+++ lib/src/shared/widgets/gradient_button.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:lyxa_live/src/core/resources/app_colors.dart';
+import 'package:lyxa_live/src/core/styles/app_styles.dart';
+
+class GradientButton extends StatefulWidget {
+  final VoidCallback onTap;
+  final String text;
+  final IconData icon;
+  final double borderRadius;
+  final List<Color> gradientColors;
+  final EdgeInsets padding;
+
+  const GradientButton({
+    super.key,
+    required this.onTap,
+    required this.text,
+    required this.icon,
+    this.borderRadius = 20.0,
+    this.gradientColors = const [Colors.blue, Colors.purple],
+    this.padding = const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+  });
+
+  @override
+  State<GradientButton> createState() => _GradientButtonState();
+}
+
+class _GradientButtonState extends State<GradientButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 100),
+      lowerBound: 0.95,
+      upperBound: 1.0,
+    );
+    _scaleAnimation = _animationController.drive(
+      Tween<double>(begin: 1.0, end: 0.95),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _onTapDown() {
+    _animationController.reverse();
+  }
+
+  void _onTapUp() {
+    _animationController.forward();
+    widget.onTap();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) => _onTapDown(),
+      onTapUp: (_) => _onTapUp(),
+      onTapCancel: () => _animationController.forward(),
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: widget.gradientColors,
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(widget.borderRadius),
+          ),
+          padding: widget.padding,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                widget.text,
+                style: AppStyles.buttonTextPrimary.copyWith(
+                  color: AppColors.whiteLight,
+                ),
+              ),
+              const SizedBox(width: 8),
+              Icon(
+                widget.icon,
+                color: Colors.white,
+                size: 26,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
