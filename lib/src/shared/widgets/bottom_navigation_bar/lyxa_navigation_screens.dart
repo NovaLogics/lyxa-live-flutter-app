fee077b386411c799ec35831433814f0fc85e9b0
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-07 17:59:40 +0530

    refactor [ features/post ]: Update Project Structure > [ Stage 8: v 18 ]

--- lib/src/shared/widgets/bottom_navigation_bar/lyxa_navigation_screens.dart
+++ lib/src/shared/widgets/bottom_navigation_bar/lyxa_navigation_screens.dart
@@ -7,7 +7,7 @@ import 'package:lyxa_live/src/core/constants/resources/app_colors.dart';
 import 'package:lyxa_live/src/core/constants/resources/app_dimensions.dart';
 import 'package:lyxa_live/src/features/auth/domain/entities/app_user_entity.dart';
 import 'package:lyxa_live/src/features/home/presentation/screens/home_screen.dart';
-import 'package:lyxa_live/src/features/post/ui/screens/upload_post_screen.dart';
+import 'package:lyxa_live/src/features/post/presentation/screens/upload_post_screen.dart';
 import 'package:lyxa_live/src/features/profile/cubits/self_profile_cubit.dart';
 import 'package:lyxa_live/src/features/profile/ui/screens/self_profile_screen.dart';
 import 'package:lyxa_live/src/features/search/ui/screens/search_screen.dart';
